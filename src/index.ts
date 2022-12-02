6913914ccd72b5e450be6f9d72a0529da066d419
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2022-12-02 15:13:54 -0500

    feat: fo-1359: add more core exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { getLedger, getNodeLedger, getWebLedger } from './services/ledger/ledgerWrapper';
+export { getLedger, getNodeLedger, getWebLedger, Ledger } from './services/ledger/ledgerWrapper';
+
+export { LedgerForNode } from './services/ledger/nodeLedger';
+export { LedgerForWeb } from './services/ledger/webLedger';
+
 export * as ledgerTypes from './services/ledger/types';
 export * as utils from './services/utils';
 export * as rangerHelper from './services/rangeHelper';
